c27a42331014bdd8bba232dc702ca77b016aa2b6
dannysatria <96224638+dannysatria@users.noreply.github.com>
2021-12-16 18:33:39 +0800

    Update modules.c

--- utils/modules.c
+++ utils/modules.c
@@ -189,17 +189,16 @@ int sorting_by_suara_sah (struct anggota_dpr_t ***tabel_anggota, int length) {
 
 	// SORTING HERE
 	struct anggota_dpr_t tmp;
-	int tabel_anggota[], int low, int high{
-   		int pivot = tabel_anggota[high];
-    		int i = (low - 1);
-    		for (int j = low; j <= high- 1; j++){
-			if (tabel_anggota[j] <= pivot){
-                		i++;
-                		swap(&tabel_anggota[i, &tabel_anggota[j]);
-            		}
-        	}   
-        	swap(&tabel_anggota[i + 1], &tabel_anggota[high]);
-    	}
+    	for (int i = 0; i < length - 1; i++) {
+        	for (int j = 0; j < length - 1 - i; j++) {
+            		if (array[j]->suara_sah > array[j + 1]->suara_sah) {
+                		tmp = array[j];
+                		array[j] = array[j + 1];
+                		array[j + 1] = tmp;
+            		};
+        	};
+    	};
+	
 	return flag;
 };
 
